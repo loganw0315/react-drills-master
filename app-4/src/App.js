2c817adf8d003d1cdf7db2b39759c5405a10f528
Logan Willis <loganw0315@gmail.com>
2021-12-16 15:00:34 -0600

    Finished app-4

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userName, setUserName] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const onChangeName = (e) => {
+    setUserName(e.target.value);
+  }
+
+  const onChangePass = (e) => {
+    setUserPassword(e.target.value);
+  }
+
+  const alertLogin = () => {
+    alert(`Username: ${userName}, Password: ${userPassword}`);
+  }
+
+
   return (
     <div className="App">
-     
+     <input type="text" onChange={onChangeName}/>
+     <input type="text" onChange={onChangePass}/>
+     <button onClick={alertLogin}>Login</button>
     </div>
   );
 }
