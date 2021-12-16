2c387663a875bd559df612e4a16bdf1552df2eb8
Logan Willis <loganw0315@gmail.com>
2021-12-16 14:32:54 -0600

    Finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [arr, setArr] = useState(['fish', 'bear', 'bird', 'cow', 'dog'])
+  const [filter, setFilter] = useState('')
+  
+  const onChangeHandler = (e) => {
+    setFilter(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input type="text" onChange={onChangeHandler}/>
+     <>
+     {arr.filter(elem=>elem.includes(filter)).map(e=><h2>{e}</h2>)}
+     </>
+     
     </div>
   );
 }
